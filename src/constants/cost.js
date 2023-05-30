d562005e39e6a5439526572cca62d953b4ae1c44
Huy Cuong <cuong@atechpartner.com>
2023-05-30 23:05:47 +0700

    no message

--- src/constants/cost.js
+++ src/constants/cost.js
@@ -1,3 +1,3 @@
-export const costInDay = 1.7;
-export const costInMonth = 1.7;
-export const costInYear = 1.7;
+export const costInDay = 0.00172;
+export const costInMonth = 0.00172;
+export const costInYear = 0.00172;
